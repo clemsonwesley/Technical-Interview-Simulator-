8d3d955f3fd97236b3e40be2c86e3f3b2dceb43d
clemsonwesley <wesleyknight99@gmail.com>
2018-10-18 00:32:05 -0400

    Finished Splash Screen + Audio
    Created Splash screen with fading in and out ability. Edited logoScript.cs to change alpha in seconds intervals. Created audio file based off of haunting ambience niose mp4

--- Virtual Reality Interview/Assets/logoScript.cs
+++ Virtual Reality Interview/Assets/logoScript.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+
+/*Script Info://
+*
+* Script will be used to fade Group 9 Logo in and out
+* Also it will be used to manage sound bit music
+*
+*/
 public class logoScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public Image logo;
+	public string loadNext;
+
+	IEnumerator Start(){
 	
-	// Update is called once per frame
-	void Update () {
-		
+		logo.canvasRenderer.SetAlpha(0.0f);
+
+		FadeIn();
+		yield return new WaitForSeconds(2.5f);
+		FadeOut();
+		yield return new WaitForSeconds(2.5f);
+		SceneManager.LoadScene(loadNext);
+
 	}
+
+
+	void FadeIn(){
+	
+		logo.CrossFadeAlpha(1.0f, 1.5f, false);
+
+	}
+
+	void FadeOut(){
+	
+		logo.CrossFadeAlpha(0.0f, 2.5f, false);
+
+	}
+
+
 }
