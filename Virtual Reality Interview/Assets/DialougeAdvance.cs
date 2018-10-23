5574803e87ce63b625e9c4198ae2dbb8a6c1e650
bwilliams97 <37247809+BWilliams97@users.noreply.github.com>
2018-10-23 19:55:33 -0400

    dialoughe

---
+++ Virtual Reality Interview/Assets/DialougeAdvance.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialougeAdvance : MonoBehaviour
+{
+
+    public Text myText;
+    private int i;
+    private string str;
+    public AudioClip logoFX;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+        myText.text = "";
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+            myText.text = "";
+            source.PlayOneShot(logoFX, 1);
+            StartCoroutine("Dialouge");
+
+        }
+    }
+
+    IEnumerator Dialouge()
+    {
+        i++;
+        str = "Test text " + i.ToString();
+
+ 
+
+        yield return new WaitForSeconds(5f);
+
+        myText.text = str;
+
+    }
+}
