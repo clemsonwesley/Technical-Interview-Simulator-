e764362ec03f6e6cf24b87ae569ce2d0e8b3c066
clemsonwesley <wesleyknight99@gmail.com>
2018-10-26 14:56:42 -0400

    Resolution Change + Volume Change

---
+++ Virtual Reality Interview/Assets/UI Scripts/resolutionChng.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resolutionChng : MonoBehaviour {
+
+	public int width;
+	public int height;
+
+	public void SetWidth(int newWidth){
+		width = newWidth;
+	}
+
+	public void SetHeight(int newHeight){
+		height = newHeight;
+	}
+
+	public void SetRes(){
+		Debug.Log("Made it");
+		Screen.SetResolution(width, height, false);
+	}
+}
