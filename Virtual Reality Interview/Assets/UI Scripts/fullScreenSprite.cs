98eb46df14c541005767c9adf0fbc312faf01fd2
clemsonwesley <wesleyknight99@gmail.com>
2018-12-05 04:07:46 -0500

    NewUI Navigation Completed
    Can now navigate from main menu to interview, options, and exit the game. Can also navigate from options back to main menu

--- Virtual Reality Interview/Assets/UI Scripts/fullScreenSprite.cs
+++ Virtual Reality Interview/Assets/UI Scripts/fullScreenSprite.cs
@@ -14,10 +14,10 @@ public class fullScreenSprite : MonoBehaviour {
 	void Update(){
 
 		string str = EventSystem.current.currentSelectedGameObject.name;
-		if(Input.GetKeyDown("space") && str == "StartButton"){
+		if(OVRInput.Get(OVRInput.Button.One) && str == "StartButton"){
 		   SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		}
-		else if(Input.GetKeyDown("space") && str == "ExitButton"){
+		else if(OVRInput.Get(OVRInput.Button.One) && str == "ExitButton"){
 			Debug.Log("Player has quit the game.");
 			Application.Quit();
 		}
---
+++ Virtual Reality Interview/Assets/UI Scripts/menuOptionQuit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuOptionQuit : MonoBehaviour {
+
+	public void QuitGame(){
+		Debug.Log("Player has quit the game.");
+		Application.Quit();
+	}
+}
