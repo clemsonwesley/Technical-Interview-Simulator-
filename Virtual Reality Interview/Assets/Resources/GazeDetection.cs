2679594eb9fe4bb88cc48243e512ffb3fb049961
clemsonwesley <wesleyknight99@gmail.com>
2018-12-05 02:17:50 -0500

    Main Menu Finalized
    Gaze Pointer to correct button is working with highlighting ability. Select A on oculus rift touch remote to continue to interveiw

---
+++ Virtual Reality Interview/Assets/Resources/GazeDetection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GazeDetection : MonoBehaviour {
+
+	public UnityEngine.UI.Button btn;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GazeEnter()
+	{
+	
+		btn.Select();
+
+	}
+
+	public void GazeExit()
+	{
+	
+		//Debug.Log("Exit");
+
+	}
+}
