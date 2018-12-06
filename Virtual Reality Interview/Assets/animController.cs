ef281329d62d44cb9fcf62b798538e87a1b697a4
jdhartl <38169608+jdhartl@users.noreply.github.com>
2018-12-05 21:25:07 -0500

    second animation

--- Virtual Reality Interview/Assets/animController.cs
+++ Virtual Reality Interview/Assets/animController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class animController : MonoBehaviour {
 
     public Animator anim;
+    System.Random rnd = new System.Random();
+    int animRand;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +17,20 @@ public class animController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
         if (Input.GetKeyDown("space"))
         {
-            anim.Play("man01_Talk01");
+            animRand = rnd.Next(1, 3);
+            switch (animRand)
+            {
+                case 1:
+                    anim.Play("man01_Talk01");
+                    break;
+                case 2: anim.Play("man01_Talk02");
+                    break;
+            }
+            
         }
 
 		
