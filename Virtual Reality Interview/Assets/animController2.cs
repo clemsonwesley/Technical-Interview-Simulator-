850f8a2fa93d11ec3124668f450748729c8933d8
jdhartl <38169608+jdhartl@users.noreply.github.com>
2018-12-06 23:53:51 -0500

    fdsfa

---
+++ Virtual Reality Interview/Assets/animController2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class animController2 : MonoBehaviour {
+
+    public Animator anima;
+    int animRand;
+
+    // Use this for initialization
+    void Start () {
+        anima = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (OVRInput.Get(OVRInput.Button.One) || Input.GetKeyDown("space"))
+        {
+            animRand = new System.Random().Next(1, 2);
+            switch (animRand)
+            {
+                case 1:
+                    anima.Play("man02_Talk01");
+                    break;
+            }
+        }
+    }
+}
