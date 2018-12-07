21289627de058dab44fb8bbe7ab29546964922e4
Isaac McQueen <mcqueen@Isaacs-MacBook-Pro.local>
2018-12-07 13:32:06 -0500

    Isaac

---
+++ Virtual Reality Interview/Assets/Vid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Vid : MonoBehaviour {
+
+
+	public void PlayGame(){
+		WaitForSeconds wait = new WaitForSeconds(3);
+		SceneManager.LoadScene(4);
+	}
+}
